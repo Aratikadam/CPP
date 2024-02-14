687358efafe7a7e8ebac70131513375846dcbc03
Aratikadam <95737222+Aratikadam@users.noreply.github.com>
2024-02-14 07:24:00 +0530

    Add files via upload

---
+++ datatypes.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	char b;
+	float c;
+	double d;
+	long e;
+	cin>>a;
+	cin>>b;
+	cin>>c;
+	cin>>d;
+	cin>>e;
+	cout<<a<<endl;
+	cout<<b<<endl;
+	cout<<c<<endl;
+	cout<<d<<endl;
+	cout<<e<<endl;
+	return 0;
+}
