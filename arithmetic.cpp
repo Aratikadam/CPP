18f971fe24a036d5efbca7003350d95d7b98103d
Aratikadam <95737222+Aratikadam@users.noreply.github.com>
2024-02-15 22:00:16 +0530

    Add files via upload

---
+++ arithmetic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int a,b;
+ cout<<"Enter any two numbers=\n";
+ cin>>a>>b;
+ cout<<"\nAddition= "<<a+b;
+ cout<<"\nSubtraction= "<<a-b;
+ cout<<"\nMultiplication= "<<a*b;
+ cout<<"\nDivision= "<<a/b;
+ return 0;
+}
