5e668adebcc3a9fa5ec26425bcfce93d7671cee6
Aratikadam <95737222+Aratikadam@users.noreply.github.com>
2024-02-16 23:51:01 +0530

    Add files via upload
    Program to check the given number is positive or negative or zero.

---
+++ isnumzero.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int a;
+ cout<<"Enter any number= ";
+ cin>>a;
+ if(a<0)
+ {
+  cout<<"No. is negative";
+ }
+ else if(a>0)
+ {
+  cout<<"No. is positive";
+ }
+ else
+ {
+  cout<<"No. is zero";
+ }
+ return 0;
+}
