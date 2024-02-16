a732caed4bd7d9d0b673d687a1c59385c4982db8
Aratikadam <95737222+Aratikadam@users.noreply.github.com>
2024-02-16 23:53:50 +0530

    Add files via upload

---
+++ num_greater_than_10.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int a;
+ cout<<"Enter any no.= ";
+ cin>>a;
+ if(a<10)
+ {
+  cout<<"No. is less than 10";
+ }
+ else
+ {
+  cout<<"No. is greater than 10";
+ }
+ return 0;
+}
