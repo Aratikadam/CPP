b4188c08ab81052b54d0940e4808582265207948
Aratikadam <95737222+Aratikadam@users.noreply.github.com>
2024-02-16 23:55:56 +0530

    Add files via upload

---
+++ vowel_consonent.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ char a;
+ cout<<"Enter any character= ";
+ cin>>a;
+ if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U')
+ {
+  cout<<"This is a vowel";
+ }
+ else
+ {
+  cout<<"This is consonent";
+ }
+ return 0;
+}
