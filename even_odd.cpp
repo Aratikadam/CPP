0acbc2ba1042e1e8ba842e9c60436646c25cf43f
Aratikadam <95737222+Aratikadam@users.noreply.github.com>
2024-02-20 20:37:00 +0530

    Add files via upload

---
+++ even_odd.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int i;
+ cout<<"Enter any No.= ";
+ cin>>i;
+ if(i%2==0)
+ {
+  cout<<"Even Number.";
+ }
+ else
+ {
+  cout<<"Odd Number.";
+ }
+ return 0;
+}
