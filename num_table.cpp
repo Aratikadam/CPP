83cd7f0b1d2bed99fe054bd88111b989fefb0913
Aratikadam <95737222+Aratikadam@users.noreply.github.com>
2024-02-20 20:49:21 +0530

    Add files via upload

---
+++ num_table.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int a=1,b,c;
+ cout<<"Enter the number= ";
+ cin>>b;
+ while(a<=10)
+ {
+  c=a*b;
+  cout<<"\n"<<c;
+  a++;
+ }
+ return 0;
+}
