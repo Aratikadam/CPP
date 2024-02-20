e516105cfcbe9caebdcadfa7b135f030089f8f4c
Aratikadam <95737222+Aratikadam@users.noreply.github.com>
2024-02-20 20:34:52 +0530

    Add files via upload

---
+++ leapyear.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int i;
+ cout<<"Enter any year= ";
+ cin>>i;
+ if(i%4==0)
+ {
+  cout<<"This is a leap year";
+ }
+ else
+ {
+  cout<<"This is not a leap year";
+ }
+ return 0;
+}
