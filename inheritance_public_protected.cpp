f95f25c8c2f80f263639eae7dd98ec000a1b39a7
Aratikadam <95737222+Aratikadam@users.noreply.github.com>
2024-04-16 10:44:50 +0530

    Add files via upload
    If we define public data members in base class and we use access mode as protected during defining derived class then data members are accessed in protected mode in derived class

---
+++ inheritance_public_protected.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+class Person
+{
+	public:
+	string name;
+	string address;
+	int age;	
+};
+class Employee: protected Person
+{
+	public:
+	int id;
+	int salary;
+	public: void getData()
+	{
+		cout<<"Enter your name: "<<endl;
+		cin>>name;
+		cout<<"Enter your age: "<<endl;
+		cin>>age;
+		cout<<"Enter your address: "<<endl;
+		cin>>address;
+		cout<<"Enter your id: "<<endl;
+		cin>>id;
+		cout<<"Enter your salary: "<<endl;
+		cin>>salary;
+	}
+	public: void displayData()
+	{
+		cout<<"Name: "<<name<<endl;
+		cout<<"Age: "<<age<<endl;
+		cout<<"Address: "<<address<<endl;
+		cout<<"Id: "<<id<<endl;
+		cout<<"Salary: "<<salary<<endl;
+	}
+};
+int main()
+{
+	Employee emp;
+	emp.getData();
+	emp.displayData(); 
+	return 0;
+}
